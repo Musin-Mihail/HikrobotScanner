7281b8f76dc4772947df3074dff798d8ab7d6360
Мусин Михаил <musin.mihail@gmail.com>
2025-11-10 19:31:48 +0300

    Снятие защиты

--- HikrobotScanner/App.xaml.cs
+++ HikrobotScanner/App.xaml.cs
@@ -32,21 +32,6 @@ namespace HikrobotScanner
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-
-            DateTime expirationDate = new DateTime(2025, 10, 25);
-
-            if (DateTime.Now > expirationDate)
-            {
-                MessageBox.Show(
-                    "Срок действия этой версии программы истек. Пожалуйста, свяжитесь с разработчиком.",
-                    "Срок действия истек",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-
-                Application.Current.Shutdown();
-                return;
-            }
-
             var mainWindow = new MainWindow
             {
                 DataContext = ServiceProvider.GetRequiredService<MainViewModel>()
